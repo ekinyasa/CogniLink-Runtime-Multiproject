65f5926e3a9bc75bb87bbb509999087214694c1f
ekinyasa <ekinyasa@gmail.com>
2026-08-24 22:08:39 +0300

    chore: remove scratch files

--- check_config.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-import { Miniflare } from "miniflare";
-
-// I can't easily run miniflare here without full setup.
-// Let's just output a small script to query the KV using Wrangler.
--- patch_l_js.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('functions/l/[slug].js', 'utf8');
-
-const target = `intentDestinations: campaign?.destinations || campaign?.routing?.destinations || null,`;
-const replacement = `intentDestinations: Object.keys(campaign?.destinations || campaign?.routing?.destinations || {}).length > 0 || (landing?.destinations && Object.keys(landing.destinations).length > 0) ? Object.assign({}, campaign?.destinations || campaign?.routing?.destinations || {}, landing?.destinations || {}) : null,`;
-
-code = code.replace(target, replacement);
-fs.writeFileSync('functions/l/[slug].js', code);
-console.log("l/[slug].js patched");
